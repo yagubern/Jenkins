eeb6d318a5a0b3e4a1bfae9e2345da4d5bb234ac
Yaroslav Gubernatorov <ygubernatoro@avaya.com>
2021-10-12 17:26:07 +0300

    Updated test

--- src/test/java/hello/TaskRestControllerTest.java
+++ src/test/java/hello/TaskRestControllerTest.java
@@ -2,25 +2,47 @@ package hello;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.assertEquals;
+import java.net.URI;
 
-public class TaskRestControllerTest extends AbstractTest {
-    @Override
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TaskRestController.class)
+@WebAppConfiguration
+public class TaskRestControllerTest {
+
+    @InjectMocks
+    private TaskRestController taskRestController;
+
+    private MockMvc mockMvc;
+
     @Before
-    public void setUp() {
-        super.setUp();
+    public void setup() {
+        initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(taskRestController).build();
     }
     @Test
     public void getUsdRate() throws Exception {
-        String uri = "/api/rate/usd/";
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
-        int status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
+        //MvcResult result = mockMvc.perform(get(URI.create("/api/rate/USD/"))).andReturn();
+
+        //String content = result.getResponse().getContentAsString();
+
+        //System.out.println(content);
+	mockMvc.perform(get("/api/rate/USD/").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
 }
